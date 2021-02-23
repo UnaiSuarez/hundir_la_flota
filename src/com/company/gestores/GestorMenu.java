794cfc333a914db3e797954d720fc79854de356c
ByChunai <unai64535@gmail.com>
2021-02-23 14:16:13 +0100

    desarrollo menu

---
+++ src/com/company/gestores/GestorMenu.java
@@ -0,0 +1,12 @@
+package com.company.gestores;
+
+import java.util.Scanner;
+
+public class GestorMenu {
+    public void menu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("OPCIONES");
+        System.out.println("________");
+        System.out.println("1) Modo de juego\n2) Ver partidas");
+    }
+}
